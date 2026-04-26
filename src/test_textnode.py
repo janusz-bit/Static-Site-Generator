1a92d5ccf509b6674adab2a70b8425bbb9faed6d
janusz-bit <janusz-bit@proton.me>
2026-04-26 07:18:57 +0200

    Split Delimiter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import TextNode, TextType, split_nodes_delimiter, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -69,6 +69,66 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             html_node.props,
             {"src": "https://example.com/image.png", "alt": "Alt text"},
+        )
+
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ],
+        )
+
+    def test_split_nodes_delimiter_bold(self):
+        node = TextNode("This is text with a **bold** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" word", TextType.TEXT),
+            ],
+        )
+
+    def test_split_nodes_delimiter_at_start(self):
+        node = TextNode("`code block` then text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("code block", TextType.CODE),
+                TextNode(" then text", TextType.TEXT),
+            ],
+        )
+
+    def test_split_nodes_delimiter_at_end(self):
+        node = TextNode("text then `code block` status", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("text then ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" status", TextType.TEXT),
+            ],
+        )
+
+    def test_split_nodes_delimiter_bold_double(self):
+        node = TextNode("word **bold** word **bold**", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("word ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" word ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+            ],
         )
 
 
